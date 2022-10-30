7b3627cfe8372747856560ed57031f5b47c85f0e
rmdhnz <ramadhanfadhly2@gmail.com>
2022-10-30 22:47:59 +0700

    mengubah konsep

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const menuSidebar = document.querySelectorAll(".sidebar ul li a");
-const mainContent = document.querySelectorAll("main .main-content");
-for (let i = 0; i < menuSidebar.length; ++i) {
-  menuSidebar[i].addEventListener("click", (e) => {
-    e.preventDefault();
-    mainContent.forEach((isi) => {
-      if (!isi.classList.contains("d-none")) {
-        isi.classList.add("d-none");
-      }
-    });
-    mainContent[i].classList.remove("d-none");
-  });
-}
+// const menuSidebar = document.querySelectorAll(".sidebar ul li a");
+// const mainContent = document.querySelectorAll("main .main-content");
+// for (let i = 0; i < menuSidebar.length; ++i) {
+//   menuSidebar[i].addEventListener("click", (e) => {
+//     e.preventDefault();
+//     mainContent.forEach((isi) => {
+//       if (!isi.classList.contains("d-none")) {
+//         isi.classList.add("d-none");
+//       }
+//     });
+//     mainContent[i].classList.remove("d-none");
+//   });
+// }
 
 const humbergerMenu = document.querySelector("main nav .humberger-menu");
 const sideBar = document.querySelector(".sidebar");
