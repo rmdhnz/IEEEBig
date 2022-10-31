9b47c32118aa536d5c2cd90fa8e55e5957fa0b73
rmdhnz <ramadhanfadhly2@gmail.com>
2022-10-31 16:23:11 +0700

    menambah padding botttom

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// const menuSidebar = document.querySelectorAll(".sidebar ul li a");
-// const mainContent = document.querySelectorAll("main .main-content");
-// for (let i = 0; i < menuSidebar.length; ++i) {
-//   menuSidebar[i].addEventListener("click", (e) => {
-//     e.preventDefault();
-//     mainContent.forEach((isi) => {
-//       if (!isi.classList.contains("d-none")) {
-//         isi.classList.add("d-none");
-//       }
-//     });
-//     mainContent[i].classList.remove("d-none");
-//   });
-// }
-
 const humbergerMenu = document.querySelector("main nav .humberger-menu");
 const sideBar = document.querySelector(".sidebar");
 humbergerMenu.addEventListener("click", (e) => {
@@ -26,7 +12,6 @@ menuSidebar.forEach((e) => {
     alert("Coming soon");
   });
 });
-
 const cardMenu = document.querySelectorAll(".card a");
 cardMenu.forEach((e) => {
   e.addEventListener("click", (isi) => {
